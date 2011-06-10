2815f2d7c51cd8002ee7c70aa77f33a61fe882e8
VincenzoGuastella <guast>
2011-06-10 14:37:36 +0200

    Vers 0.1.3 Added zoom and center functions. Added performance improvements

---
+++ src/mandelbrotsetdesigner/PixelsCalculation.scala
@@ -0,0 +1,86 @@
+package mandelbrotsetdesigner
+
+import mandelbrotsetdesigner.util._
+import scala.actors.Actor
+import scala.actors.Actor._
+import scala.collection.mutable.LinkedList
+import java.awt.image.BufferedImage
+import java.awt.{Toolkit, Color, Graphics}
+import scala.collection.mutable.ListBuffer
+
+class PixelsCalculation(imgDrawer: ImageDrawer) 
+	extends Actor with MyLoggable with Config {
+
+	var status_ = "Starting"
+		
+	def status = status_
+	
+	def act() {
+		status_ = "Started"
+		log("PixelsCalculation thread running", FINE)  
+		
+	  var functionIterators = new ListBuffer[FunctionIterator]
+		
+		var p_y , p_y0 = 0;
+	  while(p_y < HEIGHT) {
+	  	p_y0 = p_y
+	  	p_y += LINES_PER_THREAD
+	  	if (p_y >= HEIGHT) p_y = HEIGHT - 1
+	  	createIterationThread(p_y0, p_y, imgDrawer, functionIterators)
+	  	p_y += 1
+	  }
+		
+	  waitForAllThreadsCompleted(functionIterators)
+		status_ = "Completed"	  
+		log("PixelsCalculation thread Completed", VERBOSE)  
+
+		exit()
+	}
+
+		
+	/**
+	 * createIterationThread() creates a pixel calculation threads
+	 * and adds it to the active threads list
+	 * The number of threads will depend on the MAX_THREADS configuration
+	 * 
+	 * @param p_y0 the starting line in the image for 
+	 *             which calculate the colors
+	 * @param p_y1 the last line in the image for 
+	 *             which calculate the colors
+	 * @param imgDrawer the thread to which the 
+	 *                  calculation result must be sent
+	 * @param functionIterators the list holding the  
+	 *                  				active threads
+	 *             
+	 * This method throttles the calculation threads.
+	 * No more than MAX_THREADS are running concurrently.           
+	 */
+	def createIterationThread(p_y0: Int, p_y1: Int, imgDrawer: ImageDrawer, 
+														functionIterators: ListBuffer[FunctionIterator]){
+		
+  	if (functionIterators.length < MAX_THREADS) {
+  		log("Creating new actor. Range: y0[" + p_y0 + "]" + " y1[" + p_y1 + "]", FINE)
+  		var f = new FunctionIterator(p_y0, p_y1, imgDrawer, this)
+  		functionIterators.append(f)
+  	}	else {
+			receive {  
+			  case fiCompleted: FICompleted => {
+			  	functionIterators.remove(functionIterators.indexOf(fiCompleted.fi))
+			  	createIterationThread(p_y0, p_y1, imgDrawer, functionIterators)
+			  }
+			}
+  	}		
+	}
+
+	
+	def waitForAllThreadsCompleted(functionIterators: ListBuffer[FunctionIterator]) {
+		while (functionIterators.length > 0) {
+			receive {
+			  case fiCompleted: FICompleted => {
+			  	functionIterators.remove(functionIterators.indexOf(fiCompleted.fi))
+			  }
+			}
+		}	  			
+	}
+
+}
---
+++ src/mandelbrotsetdesigner/guicomponents/PopUpMenu.scala
@@ -0,0 +1,66 @@
+package mandelbrotsetdesigner.guicomponents
+
+import mandelbrotsetdesigner.util.Config
+
+import javax.swing.JPopupMenu
+import java.awt.Component
+import scala.swing.event._
+import scala.swing._
+
+trait PopUpMenu extends GuiFramework {
+	
+
+	val menu = new JPopupMenu
+	menu.add(new MenuItem(Action("Center Here"){ changeImageCenter(menu) }).peer)
+	var x = 0
+	var y = 0
+	menu.add(new Separator().peer)
+	menu.add(new Separator().peer)
+	menu.add(new MenuItem(Action("Zoom +20% (Ctrl +)"){ menu.setVisible(false); zoom(0.833333) }).peer)
+	menu.add(new MenuItem(Action("Zoom -20% (Ctrl -)"){ menu.setVisible(false); zoom(1.2)      }).peer)
+	menu.add(new Separator().peer)
+	menu.add(new Separator().peer)
+	menu.add(new MenuItem(Action("Zoom +100% (Ctrl 1)"){ menu.setVisible(false); zoom(0.5) }).peer)
+	menu.add(new MenuItem(Action("Zoom -100% (Ctrl 1)"){ menu.setVisible(false); zoom(2.0) }).peer)
+	menu.add(new Separator().peer)
+	menu.add(new Separator().peer)
+	menu.add(new MenuItem(Action("Zoom +200% (Ctrl 2)"){ menu.setVisible(false); zoom(0.25) }).peer)
+	menu.add(new MenuItem(Action("Zoom -200% (Ctrl 2)"){ menu.setVisible(false); zoom(4.0)  }).peer)
+	menu.add(new Separator().peer)
+	menu.add(new Separator().peer)
+	menu.add(new MenuItem(Action("Zoom +400% (Ctrl 4)"){ menu.setVisible(false); zoom(0.125) }).peer)
+	menu.add(new MenuItem(Action("Zoom -400% (Ctrl 4)"){ menu.setVisible(false); zoom(8.0)   }).peer)
+	menu.add(new Separator().peer)
+	menu.add(new Separator().peer)
+	menu.add(new MenuItem(Action("Zoom +800% (Ctrl 8)"){ menu.setVisible(false); zoom(0.0625) }).peer)
+	menu.add(new MenuItem(Action("Zoom -800% (Ctrl 8)"){ menu.setVisible(false); zoom(16.0)   }).peer)
+	
+	def handleMouseEvent(event: Event) {
+		    
+  	event match {
+		case MouseClicked(src, point, evModifiers, mClicks, triggerSrc) => {
+				if (evModifiers.equals(256)) { //Mouse right click
+					menu.setLocation(point.getX.toInt + 4, point.getY.toInt + 50)
+					x = point.getX.toInt + 4
+					y = point.getY.toInt + 50
+					menu.setVisible(true)
+				} else {                      //Mouse left click evModifiers == 0
+					menu.setVisible(false)
+				}
+			}
+			case e : Event => { 
+		  	log("handleMouseEvent unknown event:" + e.toString, INFO)
+		  }		  
+
+    }
+
+	}
+	
+  def changeImageCenter(menu: JPopupMenu) {
+  	Config.setCenter(x, y)
+  	menu.setVisible(false)
+  	repaintAll = true 
+  	mainFrame.repaint 
+  }
+
+}
